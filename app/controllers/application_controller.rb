f63b14a1321eb896dc5d6189e950f94cf5649af7
Jonas Peschla <github@peschla.net>
2015-12-03 23:31:26 +0100

    Minor changes on devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_params
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :name, :password, :password_confirmation, :street_name, :street_number, :postal_code, :city) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password) }
   end
 
   def redirect_to_back(options = {}, default = root_path)
