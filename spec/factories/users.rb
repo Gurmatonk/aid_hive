3cbc516228935443667513a9950ae8cf028ed026
Jonas Peschla <github@peschla.net>
2015-12-20 17:02:07 +0100

    Make user factory work. Needs way to skip sending invitation mail/create already active users.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,14 @@
 FactoryGirl.define do
   factory :user do
-    confirmed_at Time.now
-    name "Test User"
-    email "test@example.com"
-    password "please123"
+    confirmed_at -> { Time.zone.now }
+    city 'Stuttgart'
+    email 'test@example.com'
+    name 'Test User'
+    password 'please123thisissecure456'
+    postal_code '70180'
 
     trait :admin do
       role 'admin'
     end
-
   end
 end
