f88c35b6be06771117f75fcaa22322b0db3c5826
Jonas Peschla <github@peschla.net>
2015-12-20 14:09:52 +0100

    I18n for reset password instructions

--- spec/mailers/previews/aid_hive_devise_mailer_preview.rb
+++ spec/mailers/previews/aid_hive_devise_mailer_preview.rb
@@ -1,9 +1,35 @@
 class AidHiveDeviseMailerPreview < ActionMailer::Preview
-  def confirmation_instructions
-    AidHiveDeviseMailer.confirmation_instructions(User.first, "faketoken")
+  def confirmation_instructions_de
+    I18n.locale = :de
+    AidHiveDeviseMailer.confirmation_instructions(User.first, 'faketoken')
   end
 
-  def reset_password_instructions
-    AidHiveDeviseMailer.reset_password_instructions(User.first, "faketoken")
+  def confirmation_instructions_en
+    I18n.locale = :en
+    AidHiveDeviseMailer.confirmation_instructions(User.first, 'faketoken')
+  end
+
+  def reset_password_instructions_confirmed_de
+    I18n.locale = :de
+    AidHiveDeviseMailer.reset_password_instructions(User.first, 'faketoken')
+  end
+
+  def reset_password_instructions_unconfirmed_de
+    I18n.locale = :de
+    user = User.first
+    user.confirmed_at = nil
+    AidHiveDeviseMailer.reset_password_instructions(user, 'faketoken')
+  end
+
+  def reset_password_instructions_confirmed_en
+    I18n.locale = :en
+    AidHiveDeviseMailer.reset_password_instructions(User.first, 'faketoken')
+  end
+
+  def reset_password_instructions_unconfirmed_en
+    I18n.locale = :en
+    user = User.first
+    user.confirmed_at = nil
+    AidHiveDeviseMailer.reset_password_instructions(user, 'faketoken')
   end
 end
