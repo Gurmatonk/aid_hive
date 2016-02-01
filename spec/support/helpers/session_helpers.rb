69ef2b9cfe04b61f7ad3fda86f1c50716765d410
Jonas Peschla <github@peschla.net>
2016-02-01 21:58:35 +0100

    Finish non-oauth log in tests

--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -4,15 +4,15 @@ module Features
       visit new_user_registration_path
       fill_in 'Email', with: email
       fill_in 'Password', with: password
-      fill_in 'Password confirmation', :with => confirmation
+      fill_in 'Password confirmation', with: confirmation
       click_button 'Sign up'
     end
 
-    def signin(email, password)
+    def sign_in(email, password)
       visit new_user_session_path
       fill_in 'Email', with: email
       fill_in 'Password', with: password
-      click_button 'Sign in'
+      click_button 'Log in'
     end
   end
 end
