d3420d4475d7cfe6d21ecae4bff932fd9533f1f4
Jonas Peschla <github@peschla.net>
2016-02-02 00:36:00 +0100

    Fix test to pass on Semaphore

--- spec/features/users/user_delete_spec.rb
+++ spec/features/users/user_delete_spec.rb
@@ -11,10 +11,8 @@ feature 'User delete', :devise, :js do
   scenario 'user can delete his own account' do
     login_as user, scope: :user, run_callbacks: false
     visit edit_user_registration_path(user)
-    # TODO: Figure out how the captions can be fetched by I18n.t and client and server having the same language set.
-    #       Replace last expect by proper feature style test with sign_in and test for error message.
     page.accept_confirm do
-      click_button 'Mein Benutzerkonto löschen'
+      click_button I18n.t('views.devise.registrations.edit.cancel_button')
     end
     expect(User.find_by_id(user.id)).to be_nil
   end
