bd2b64a1243d502440f91aaa7c136a525ce22b7a
Jonas Peschla <github@peschla.net>
2016-02-01 22:02:14 +0100

    Adjust log out feature test

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -1,7 +1,3 @@
-# Feature: Sign in
-#   As a user
-#   I want to sign in
-#   So I can visit protected areas of the site
 feature 'Sign in', :devise do
   describe 'unregistered user' do
     scenario 'user cannot sign in if not registered' do
